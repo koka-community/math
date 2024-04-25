47344f3b3c58cc8cecf25bf7e663b81595893c3e
ki11errabbit <ki11errabbit@posteo.net>
2024-04-25 15:22:48 -0600

    doc: added documentation fix: added license header

--- math/inline/matrix.c
+++ math/inline/matrix.c
@@ -1,3 +1,11 @@
+/*----------------------------------------------------------------------------
+   Copyright 2024, Koka-Community Authors
+
+   Licensed under the MIT License ("The License"). You may not
+   use this file except in compliance with the License. A copy of the License
+   can be found in the LICENSE file at the root of this distribution.
+----------------------------------------------------------------------------*/
+
 #include <cblas.h>
 
 kk_math_matrix__blasmatrix kk_matrix_blasmatrix(kk_vector_t matrix, kk_context_t* ctx) {
